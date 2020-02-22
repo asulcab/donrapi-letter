df0b986a3693e5ef6285adba5ba80baa29c64b24
Americo <asulca99@gmail.com>
2020-02-22 01:33:42 -0500

    change icon Delivery food: Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { FaAlignRight, FaMotorcycle, FaPizzaSlice } from "react-icons/fa";
+import { FaAlignRight, FaMotorcycle, FaPizzaSlice, FaUtensils } from "react-icons/fa";
 import logo from "../images/logo.svg";
 import Contact from "./Contact";
 export default class Navbar extends Component {
@@ -36,7 +36,7 @@ export default class Navbar extends Component {
               <Link to="/nosotros">Nosotros</Link>
             </li>
             <li>
-              <Link to="/food-delivery" style={{ color: "#ffd800" }}><FaPizzaSlice className="nav-icon-delivery" />Delivery Food!</Link>
+              <Link to="/food-delivery" style={{ color: "#ffd800" }}><FaUtensils className="nav-icon-delivery" />Delivery Food!</Link>
             </li>
             {/* <li>
               <Link to="/rooms">Especialistas</Link>
